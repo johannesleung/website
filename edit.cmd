63c5cb5e5f1b846022c774265eb15540f68bb102
Enno Lohmeier <enno@nerdworks.de>
2018-07-18 22:46:03 +0200

    add scripts

---
+++ edit.cmd
@@ -0,0 +1,3 @@
+git -C C:\Users\e.lohmeier\Desktop\website pull
+START /B "C:\Program Files\Mozilla Firefox\firefox.exe" "http://localhost:5000"
+lektor --project C:\Users\e.lohmeier\Desktop\website\ server
---
+++ publish.cmd
@@ -0,0 +1,5 @@
+lektor --project C:\Users\e.lohmeier\Desktop\website\ deploy
+git -C C:\Users\e.lohmeier\Desktop\website add .
+git -C C:\Users\e.lohmeier\Desktop\website commit -m "modified"
+git -C C:\Users\e.lohmeier\Desktop\website push
+pause
