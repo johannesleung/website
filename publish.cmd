f578a8214adf083dce58052b23614b9b3fb16618
Johannes Leung <johannes.s.leung@web.de>
2018-07-19 11:00:18 +0200

    modified

--- publish.cmd
+++ publish.cmd
@@ -1,4 +1,4 @@
-lektor --project C:\Users\johannes\website deploy
 git -C C:\Users\johannes\website add .
 git -C C:\Users\johannes\website commit -m "modified"
 git -C C:\Users\johannes\website push
+lektor --project C:\Users\johannes\website deploy
